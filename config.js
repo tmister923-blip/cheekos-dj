6047d296e32f38244c522309db0801b299fcf076
cheekos1 <baszgad@gmail.com>
2025-09-01 23:27:22 -0400

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ class EnterpriseConfigurationManager {
                 host: EnvironmentVariableProcessor.LAVALINK_HOST || "140.238.255.205", 
                 port: EnvironmentVariableProcessor.LAVALINK_PORT || 8262,       
                 password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "glace", 
-                secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'true' || false
+                secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'false' || false
             },
             
             /**
@@ -101,4 +101,5 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
